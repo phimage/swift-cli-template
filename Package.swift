6fe62ef88a3a32bbd806ccb226c79e3d958ea668
Eric Marchand <phimage@users.noreply.github.com>
2022-11-10 07:05:33 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,19 +7,20 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(
-            name: "{{ tmplr.repo_name }}"",
-            targets: ["{{ tmplr.repo_name }}""])
+            name: "{{ tmplr.repo_name }}",
+            targets: ["{{ tmplr.repo_name }}"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-        .package(url: "https://github.com/{{ tmplr.owner_name}}/XcodeProjKit.git", from: "3.0.3")
+        .package(url: "https://github.com/phimage/XcodeProjKit.git", from: "3.0.3")
     ],
     targets: [
         .executableTarget(
-            name: "{{ tmplr.repo_name }}"",
+            name: "{{ tmplr.repo_name }}",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                "XcodeProjKit"]
+                "XcodeProjKit"],
+            path: "Sources/main"
         )
     ]
 )
